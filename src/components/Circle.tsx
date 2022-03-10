1b749f971e0644e82b26e635726ee855be79aebb
Brahim Berkasse <berkassebrahim@gmail.com>
2022-03-10 17:49:20 +0100

    🥩 React zoomable view 1

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,28 +1,45 @@
 import React from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
-import Animated, { useSharedValue } from "react-native-reanimated";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 interface Props {
+  name: string;
+  time: string;
   x: number;
   y: number;
   d: number;
 }
 
-export const Circle: React.FC<Props> = ({ x, y, d }) => {
+export const Circle: React.FC<Props> = ({ name, time, x, y, d }) => {
+  const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
   const offsetX = useSharedValue(x);
   const offsetY = useSharedValue(y);
   const dimension = useSharedValue(d);
 
   const gesture = Gesture.Pan();
-
+  const styled = useAnimatedStyle(() => {
+    return {
+      width: dimension.value,
+      height: dimension.value,
+      borderRadius: dimension.value,
+      transform: [
+        { perspective: 1500 },
+        { translateX: offsetX.value },
+        { translateY: offsetY.value },
+      ],
+    };
+  });
   return (
     <View pointerEvents={"box-none"}>
       <GestureDetector gesture={gesture}>
-        <Pressable style={[styles.circle]}>
-          <Text style={styles.name}>TASK 1</Text>
-          <Text style={styles.time}>01:00:02</Text>
-        </Pressable>
+        <AnimatedPressable style={[styles.circle, styled]}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.time}>{time}</Text>
+        </AnimatedPressable>
       </GestureDetector>
     </View>
   );
@@ -30,12 +47,11 @@ export const Circle: React.FC<Props> = ({ x, y, d }) => {
 
 const styles = StyleSheet.create({
   circle: {
-    height: 100,
-    width: 100,
+    //height: 100,
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: "#FFE6E6",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 100,
   },
   name: {
     fontWeight: "bold",
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,71 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Circle } from '../components'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Circle } from "../components";
+import ReactNativeZoomableView from "@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView";
 
-export const Home : React.FC = () => {
-
-  return (
-    <View>
-      <Circle />
-    </View>
-  )
-
+interface ICircle {
+  name: string;
+  x: number;
+  y: number;
+  d: number;
+  time: string;
 }
+
+const circles: ICircle[] = [
+  {
+    name: "TASK 1",
+    time: "01:00:00",
+    x: 150,
+    y: 150,
+    d: 100,
+  },
+  {
+    name: "TASK 1",
+    time: "01:00:00",
+    x: 250,
+    y: 150,
+    d: 200,
+  },
+];
+
+export const Home: React.FC = () => {
+  return (
+    <View style={{ flex: 1, width: "400%" }}>
+      {circles.map((c, key) => (
+          <Circle
+            name={c.name}
+            time={c.time}
+            d={c.d}
+            x={c.x}
+            y={c.y}
+            key={key}
+          />
+        ))}
+      {/*
+      <ReactNativeZoomableView
+        maxZoom={1.5}
+        minZoom={0.5}
+        zoomStep={0.1}
+        initialZoom={1}
+        //bindToBorders={true}
+        movementSensibility={1}
+        style={{
+          padding: 10,
+          //backgroundColor: "red",
+        }}
+      >
+        {circles.map((c, key) => (
+          <Circle
+            name={c.name}
+            time={c.time}
+            d={c.d}
+            x={c.x}
+            y={c.y}
+            key={key}
+          />
+        ))}
+      </ReactNativeZoomableView>
+      */}
+    </View>
+  );
+};
