3ded47c0713c1016958ad1924626707172a8bb66
Brahim Berkasse <berkassebrahim@gmail.com>
2022-03-16 21:57:04 +0100

    🪲 Reanimated : apply animated props on rect

--- src/components/Sketch/Grid.tsx
+++ src/components/Sketch/Grid.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { View, Text, Dimensions } from "react-native";
-import Svg, { Path, Line } from "react-native-svg";
+import Svg, { Rect, Line } from "react-native-svg";
+import Animated, {
+  useAnimatedProps,
+  useSharedValue,
+} from "react-native-reanimated";
+
+const AnimatedRect = Animated.createAnimatedComponent(Rect);
 
 const { height, width } = Dimensions.get("screen");
 const GAP = 20;
 const STROKE = 1;
 
 export const GridSketch: React.FC = () => {
+  const rectX = useSharedValue(24);
+  const rectY = useSharedValue(64);
+
+  const rectStyle = useAnimatedProps(() => {
+    return {
+      x: rectX.value,
+      y: rectY.value,
+    };
+  });
+
   const generateHorizontalLines = (): number[] => {
     const points: number[] = [];
     for (let i = 1; i < height / (GAP + STROKE); i++) {
@@ -21,6 +37,7 @@ export const GridSketch: React.FC = () => {
     }
     return points;
   };
+  const moveRect = () => {};
   return (
     <View style={{ flex: 1 }}>
       <Svg height={height} width={width} style={{ backgroundColor: "black" }}>
@@ -46,6 +63,14 @@ export const GridSketch: React.FC = () => {
             y2={height}
           />
         ))}
+        <AnimatedRect
+          animatedProps={rectStyle}
+          stroke={"white"}
+          strokeWidth={STROKE}
+          height="21"
+          width="21"
+          fill="white"
+        />
       </Svg>
     </View>
   );
